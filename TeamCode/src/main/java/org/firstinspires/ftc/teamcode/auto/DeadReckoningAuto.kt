eb512473f4629a4c46394f36b8eeebbb4831a58d
nathanlesser <nathanlesser@outlook.com>
2018-01-16 16:33:00 -0500

    Jacob was here

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/DeadReckoningAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/DeadReckoningAuto.kt
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.DcMotor
 import org.firstinspires.ftc.teamcode.hardware.Rebound
 
-@Autonomous(name = "Park Red Near", group = "Dead Reckoning")
+@Autonomous(name = "So Auto Didn't Save...")
 //@Disabled
 class DeadReckoningAuto : LinearOpMode() {
 
@@ -20,22 +20,7 @@ class DeadReckoningAuto : LinearOpMode() {
         val imu : BNO055IMU = hardwareMap.get(BNO055IMU::class.java, "imu")
         drivetrain = Rebound(this, lf, lb, rf, rb, imu)
         drivetrain.initialize(DcMotor.RunMode.RUN_USING_ENCODER, DcMotor.ZeroPowerBehavior.BRAKE)
-        //val mediaPlayer: MediaPlayer = MediaPlayer.create(hardwareMap.appContext, R.raw.up_and_away)
-        //mediaPlayer.start()
         waitForStart()
-        drivetrain.arcadeMecanum(0.0, -0.35, 0.0)
-        sleep(1200)
-        drivetrain.stopMoving()
-        sleep(1000)
-        drivetrain.arcadeMecanum(-0.7, 0.0, 0.85)
-        sleep(1000)
-        drivetrain.stopMoving()
-        sleep(500)
-        drivetrain.arcadeMecanum(0.35, -0.35, 0.0)
-        sleep(1000)
-        drivetrain.stopMoving()
-        //mediaPlayer.stop()
-
     }
 
 
